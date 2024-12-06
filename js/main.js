8ed0bf69d5a9dd324170d411623f9ae62bdf987a
Davidle09 <144161548+Davidle09@users.noreply.github.com>
2024-12-06 06:18:27 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+       $(".category-card").hover(
+function() {
+	$(this).css("opacity", "0.5");
+},
+function() {
+	$(this).css("opacity", "1");
+});
+});// JavaScript Document
